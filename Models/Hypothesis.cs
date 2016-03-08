b2322acf9b892ea209870680362b6e9c83c01b5b
martin.grihangne <martin.grihangne@eway.com.au>
2016-03-09 07:57:58 +1100

    Models for the Layer, NeuralNetwork and Hypothesis

---
+++ Models/Hypothesis.cs
@@ -0,0 +1,15 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using Vec = MathNet.Numerics.LinearAlgebra.Vector<float>;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MnistDigits.Models
+{
+    public interface Hypothesis
+    {
+        float Value(Vec sample);
+    }
+}
---
+++ Models/Layer.cs
@@ -0,0 +1,28 @@
+﻿using MathNet.Numerics.LinearAlgebra;
+using Mat = MathNet.Numerics.LinearAlgebra.Matrix<float>;
+using Vec = MathNet.Numerics.LinearAlgebra.Vector<float>;
+using Mb = MathNet.Numerics.LinearAlgebra.MatrixBuilder<float>;
+using Vb = MathNet.Numerics.LinearAlgebra.VectorBuilder<float>;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MnistDigits.Models
+{
+    public class Layer
+    {
+        private static Mb _M = Mat.Build;
+        private static Vb _V = Vec.Build;
+
+        private Matrix<float> weights;
+
+        public Layer(int weightsRows, int weightsColumns)
+        {
+            weights = _M.Random(weightsRows, weightsColumns);
+        }
+
+    }
+
+}
